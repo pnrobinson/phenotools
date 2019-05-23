17dd53dc583d3e81e243380dd68795bd8e8cda1a
pnrobinson <peter.robinson@jax.org>
2019-05-23 13:01:28 -0400

    getting there

--- python3/main.py
+++ python3/main.py
@@ -1,16 +1,24 @@
 import base_pb2 as base
 import phenopackets_pb2 as phenopackets
 from google.protobuf import json_format
+from google.protobuf.json_format import Parse, MessageToJson
 #from google.protobuf import JsonFormat
 import json
 
 ### Input phenopacket from JSON file
-phenopacket=phenopackets.Phenopacket();
+
 
 path="../Gebbia-1997-ZIC3.json"
 with open(path,'r') as f:
-    json_string=json.load(f)
-#print(json_string)
+    packet=Parse(message=phenopackets.Phenopacket(),text=f.read())
+    #print (packet)
+
+print("Reading phenopacket from {}".format(path))
+print("Subject id: {}".format(packet.subject.id))
+print("dtaypte",type(packet.subject))
+indiv = packet.subject
+print(indiv.id)
 
 
-json_format.Parse(json_string,phenopacket)
+
+print(indiv.age_at_collection.age)
