369c5a743bc41b06f06a7c6990b041854ec76141
pnrobinson <peter.robinson@jax.org>
2019-10-17 17:31:48 -0500

    cleaning up unit tsts for ontology

--- lib/tests/ontology_test.cpp
+++ lib/tests/ontology_test.cpp
@@ -37,22 +37,21 @@ TEST_CASE("Test exists path algorithm","[exists_path]") {
   TermId t4 = TermId::from_string("HP:0000004");
   // term 5 is the grand child of term 1 but not child of term 2
   TermId t5 = TermId::from_string("HP:0000005");
-  bool b = ontology->exists_path(t2,t1,EdgeType::IS_A);
+  bool b = ontology->exists_path(t2,t1);
   REQUIRE(b);
-  b = ontology->exists_path(t3,t1,EdgeType::IS_A);
+  b = ontology->exists_path(t3,t1);
   REQUIRE(b);
-  b = ontology->exists_path(t1,t3,EdgeType::IS_A);
-  b = ontology->exists_path(t5,t3,EdgeType::IS_A);
+  REQUIRE(ontology->exists_path(t1,t3, EdgeType::IS_A_INVERSE));
+  b = ontology->exists_path(t5,t3);
   REQUIRE(!b);
-  b = ontology->exists_path(t5,t4,EdgeType::IS_A);
+  b = ontology->exists_path(t5,t4);
   REQUIRE(b);
-  b = ontology->exists_path(t5,t1,EdgeType::IS_A);
+  b = ontology->exists_path(t5,t1);
   REQUIRE(b);
-  b = ontology->exists_path(t5,t2,EdgeType::IS_A);
-  REQUIRE(!b);
+  REQUIRE_FALSE(ontology->exists_path(t5,t2));
 }
 
-TEST_CASE("Test exists path algorithm woth specific edge type","[exists_path_edgetype]") {
+TEST_CASE("Test exists path algorithm with specific edge type","[exists_path_edgetype]") {
   string hp_json_path = "../testdata/hp.small.json";
   JsonOboParser parser {hp_json_path};
   std::unique_ptr<Ontology>  ontology = parser.get_ontology();
@@ -74,13 +73,17 @@ TEST_CASE("Test exists path algorithm woth specific edge type","[exists_path_edg
   REQUIRE(b);
   b = ontology->exists_path(t3, t1, EdgeType::IS_A);
   REQUIRE(b);
-  b = ontology->exists_path(t1, t3, EdgeType::IS_A);
-  b = ontology->exists_path(t5, t3, EdgeType::IS_A);
-  REQUIRE(!b);
-  b = ontology->exists_path(t5, t4, EdgeType::IS_A);
+  b = ontology->exists_path(t1, t3, EdgeType::IS_A_INVERSE);
   REQUIRE(b);
-  b = ontology->exists_path(t5, t1, EdgeType::IS_A);
-  REQUIRE(b);
-  b = ontology->exists_path(t5, t2, EdgeType::IS_A);
-  REQUIRE(!b);
+  // t3 is grand child of t1
+  REQUIRE(ontology->exists_path(t3, t1, EdgeType::IS_A));
+  // t5 is grandchild of t1 and child of t4 but not descendant of t3
+  REQUIRE_FALSE(ontology->exists_path(t5, t3, EdgeType::IS_A));
+  // t5 is child of t4
+  REQUIRE(ontology->exists_path(t5, t4, EdgeType::IS_A));
+  REQUIRE_FALSE(ontology->exists_path(t4, t5));
+  REQUIRE_FALSE(ontology->exists_path(t5, t4, EdgeType::IS_A_INVERSE));
+  REQUIRE(ontology->exists_path(t5, t1));
+  // t5 and t2 are in different subhierarchies
+  REQUIRE_FALSE(ontology->exists_path(t5, t2));
 }
